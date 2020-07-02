e53659113f500031ebe3440fcfcd784fd54a3c33
Emily Gaska <emilygaska@gmail.com>
2020-07-01 19:01:16 -0500

    Set up react app

---
+++ models/books.js
@@ -0,0 +1,14 @@
+
+const mongoose = require("mongoose"),
+  Schema = mongoose.Schema,
+  BookSchema = new Schema({
+    authors:  { type: String, required: true },
+    description:  { type: String },
+    image:  { type: String },
+    link:  { type: String, required: true },
+    title:  { type: String, required: true }
+  });
+
+const Book = mongoose.model("Book", BookSchema);
+
+module.exports = Book;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+// Add routes, both API and view
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebookdirectory");
+
+// Start the API server
+app.listen(PORT, () => {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
+  
