564160bff8b8c8cd271deda9fa9235e429ae0373
Emily Gaska <emilygaska@gmail.com>
2020-07-01 21:59:14 -0500

    Added outline for results cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "./components/header/header"
 import SearchForm from './components/searchForm/searchForm';
+import Results from './components/results/results';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <SearchForm/>
+      <Results/>
     </div>
   );
 }
--- src/components/searchForm/searchForm.js
+++ src/components/searchForm/searchForm.js
@@ -3,25 +3,30 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
-
+import Container from "react-bootstrap/Container";
+import style from "./styles.css";
 
 function SearchForm() {
   return (
-    <Form>
-      <Form.Group as={Row} controlId="formHorizontalEmail">
-        <Form.Label column sm={2}>
-          Search
-        </Form.Label>
-        <Col sm={10}>
-          <Form.Control type="test" placeholder="Search" />
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row}>
-        <Col sm={{ span: 10, offset: 2 }}>
-          <Button type="submit">Search</Button>
-        </Col>
-      </Form.Group>
-    </Form>
+    <div>
+<Container fluid>
+      <Form>
+        <Form.Group as={Row} controlId="SearchId">
+          <Form.Label column sm={10}>
+            Book Search:
+          </Form.Label>
+          <br/>
+            <Form.Control type="test" placeholder="Search" />
+         
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Col sm={{ span: 10, offset: 2 }}>
+            <Button type="submit">Search</Button>
+          </Col>
+        </Form.Group>
+      </Form>
+          </Container>
+    </div>
   );
 }
 
